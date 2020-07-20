68679377d41a6f9d61e0aeec7acb318d4c1697b5
Zenith <58347499+NPUZenith@users.noreply.github.com>
2020-07-20 18:16:37 -0400

    16_PB_CU2

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -19,10 +19,11 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // Allows for thrusting while rotating
         {
@@ -36,6 +37,11 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -45,5 +51,8 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+
     }
 }
